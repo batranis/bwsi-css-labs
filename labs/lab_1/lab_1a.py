fc6f0542a270367d55cea17b91d3f1ca956c9e9f
Nishka Batra <niksbatra09@gmail.com>
2026-06-25 03:24:24 -0700

    Update1
    Commit name

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -10,7 +10,7 @@ This is to simulate a change made on a robot: robot_speed = 8 # m/s
 def main():
     print("Hello World!")
 
-    name = "Nishka Batra" # TODO: Insert your name between the double quotes
+    name = "Nishka" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
     print(f"I like cybersecurity!")
